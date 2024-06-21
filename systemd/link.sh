9261aae3e5889a0017a33fb641f471c6d0a7dd04
takuto1127 <runner@runner.local>
2024-06-21 05:29:32 +0000

    systemd conf file init

---
+++ systemd/link.sh
@@ -0,0 +1,2 @@
+sudo ln -s /home/runner/battari-server-conf/systemd/battari-api-dev.service /etc/systemd/system/
+sudo ln -s /home/runner/battari-server-conf/systemd/battari-api-prod.service /etc/systemd/system/
